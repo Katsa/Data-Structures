2b6b40d8316a7b746a7479e773b8a65154dac7d6
deborge <deborge94@gmail.com>
2014-04-22 22:54:03 -0400

    changed animal game

--- Assignment8/src/AnimalGame.java
+++ Assignment8/src/AnimalGame.java
@@ -3,195 +3,147 @@ import java.util.Scanner;
 public class AnimalGame {
 	public static void play(String inputFile, String outputFile){
 		Scanner scan = new Scanner(System.in);
+		AnimalTree animalTree = AnimalTree.readFile(inputFile);
 		System.out.println("Would you like to play a game?(y/n)");
 		String answer = scan.nextLine();
-		//System.out.println(answer);
 		if (answer.equals("y")){
-			playGame(AnimalTree.readFile(inputFile));
-			//System.out.println(answer);
+			askQuestion(animalTree,scan,outputFile);
+			System.out.println("hey");
+			//animalTree.writeFile(outputFile);
+			play(outputFile,outputFile);
 		}
 		else if (answer.equals("n")){
-			System.out.println("Too Bad, So Sad!");
+			System.out.println("HAHA sucka, you missing out!");
 		}
 		else
-		{System.out.println("Stop Messing Around");
-		System.out.println(answer);
-		
+		{System.out.println("Stop Playin'");
+		play(inputFile,outputFile);
 		}
 	}
-
-	public static void checkWin(AnimalTree animalTree, String answer) {
-		if (answer.equals("y")){
-			System.out.println("Great");
+	
+	
+	public static void askQuestion(AnimalTree animalTree, Scanner scan,String outputFile){
+		String answer;
+		if (animalTree.isLeaf()){
+			finalGuess(animalTree,scan,outputFile);
 		}
 		else{
-			System.out.println("Something went wrong");
-			playGame(animalTree);
-		}	
+			System.out.println(animalTree.data());
+			answer = scan.next();
+			if (answer.equals("y")){
+				askQuestion(animalTree.right(),scan,outputFile);
+			}
+			else if (answer.equals("n")){
+				askQuestion(animalTree.left(),scan,outputFile);
+			}
+			else {
+				askQuestion(animalTree,scan,outputFile);
+			}
+		}
 	}
-
-	public static void checkLeaves(AnimalTree animalTree, String answer) {
+	
+	public static void finalGuess(AnimalTree animalTree, Scanner scan,String outputFile ){
+		//System.out.println(animalTree.data());
+		String answer;
+		System.out.println("Were you thinking of a " + animalTree.data() + "?" );
+		answer = scan.next();
 		if (answer.equals("y")){
-			System.out.println("Great");
+			System.out.println("Sweet!");
+			
 		}
 		else if (answer.equals("n")) {
-			wrongAnswer(animalTree);
+			wrongAnswer(animalTree, outputFile);
 		}
 		else{
-			playGame(animalTree);
+			finalGuess(animalTree,scan,outputFile);
 		}	
 	}
-	
-	
-	public static void playGame(AnimalTree animalTree){
-		Scanner scan = new Scanner(System.in);
-		String answer;
-		//System.out.println(animalTree.data() + "1");
-		if (animalTree.isLeaf()){
-			System.out.println("First");
-			System.out.println("Were you thinking of a " + animalTree.data());
-			answer = scan.next();
-			checkLeaves(animalTree, answer);
-		}
-		else if(!animalTree.left().isLeaf()){
-			System.out.println("Second");
-			System.out.println(animalTree.data());
-			answer = scan.next();
-			if (answer.equals("y")){
-				System.out.println("Were you thinking of a " + animalTree.visit(animalTree.right()));
-				answer = scan.next();
-				checkWin(animalTree, answer);
-			}
-			else if (answer.equals("n")) {
-				System.out.println(animalTree.visit(animalTree.left()));
-				answer = scan.next();
-				if(answer.equals("y")){
-					System.out.println("Were you thinking of a " + animalTree.visit(animalTree.left().right()));
-					answer = scan.next();
-					checkLeaves(animalTree, answer);
-				}
-				else if(answer.equals("n")){
-					System.out.println("Were you thinking of a " + animalTree.visit(animalTree.left().left()));
-					answer = scan.next();
-					checkWin(animalTree, answer);
-				}
-				else{
-					playGame(animalTree);
-				}	
-				
-			}
-		}
-		else {
-			System.out.println("Third");
-			System.out.println(animalTree.data());
-			answer = scan.next();
-			if (answer.equals("y")){
-				System.out.println("Were you thinking of a " + animalTree.visit(animalTree.right()));
-				answer = scan.next();
-				checkLeaves(animalTree, answer);
-			}
-			else if (answer.equals("n")) {
-				System.out.println("Were you thinking of a " + animalTree.visit(animalTree.left()));
-				answer = scan.next();
-				checkLeaves(animalTree, answer);;
-			}
-			else{
-				playGame(animalTree);
-			}	
-		}
+	public static void firstWrongAnswer (AnimalTree animalTree, String outputfile){
+		
 	}
-	public static void wrongAnswer (AnimalTree animalTree){
+	public static void wrongAnswer (AnimalTree animalTree, String outputFile){
 		Scanner scan = new Scanner(System.in);
-
 		boolean finished = false;
-
-		if (animalTree.isLeaf()){
+		System.out.println("Dang! What was the animal?");
+		String answer = scan.nextLine();
+		AnimalTree newAnimal = new AnimalTree(answer);
+		System.out.println("What question separates " + newAnimal.data() +  " from " + animalTree.data()+"?");
+		
+		if (animalTree.isRoot()){
 			System.out.println("top");
 			System.out.println("Doh! What was the animal?");
 			AnimalTree newAnimal = new AnimalTree(scan.nextLine());
 
 			System.out.println("What question separates " + newAnimal.data() +  " from " + animalTree.data()+"?");
 			String seperateQuestion = scan.nextLine();
-			
+
 			while (!finished){
 				System.out.println("What is the the correct answer for " + newAnimal.data() + "?");
 				String correctAnswer = scan.nextLine();
 				if (correctAnswer.equals("y")){
 					AnimalTree newNode = new AnimalTree(seperateQuestion,animalTree,newAnimal);
-					newNode.writeFile("tree.txt");
-					finished = true;
+					newNode.writeFile(outputFile);
+					
 				}else if(correctAnswer.equals("n")){
 					AnimalTree newNode = new AnimalTree(seperateQuestion,newAnimal,animalTree);
-					newNode.writeFile("tree.txt");
-					finished = true;
+					newNode.writeFile(outputFile);
+					
 				}else {
 					System.out.println("Just answer the stupid question!");
 				}
+				finished = true;
 			}
-		}
-		else if(!animalTree.left().isLeaf()) {
-			System.out.println("middle");
-			System.out.println("Doh! What was the animal?");
-			AnimalTree newAnimal = new AnimalTree(scan.nextLine());
-			System.out.println("What question separates " + newAnimal.data() +  " from " + animalTree.visit(animalTree.left().right())+"?");
-			String seperateQuestion = scan.nextLine();
-			while (!finished){
-				System.out.println("What is the the correct answer for " + newAnimal.data() + "?");
-				String correctAnswer = scan.nextLine();
-				if (correctAnswer.equals("y")){
-					AnimalTree oldAnimal = new AnimalTree(animalTree.visit(animalTree.left().right()));
-					AnimalTree newNode = new AnimalTree(seperateQuestion,oldAnimal,newAnimal);
-					animalTree.left().setRight(newNode);
-					animalTree.writeFile("tree.txt");
-					finished = true;
-				}else if(correctAnswer.equals("n")){
-					AnimalTree oldAnimal = new AnimalTree(animalTree.visit(animalTree.left()));
-					AnimalTree newNode = new AnimalTree(seperateQuestion,newAnimal,oldAnimal);
-					animalTree.left().setRight(newNode);
-			
-					animalTree.writeFile("tree.txt");
-					finished = true;
-				}else {
-					System.out.println("Just answer the stupid question!");
-				}
-			}
-
 		}
 		else {
-			System.out.println("bottom");
-			System.out.println("Doh! What was the animal?");
-			AnimalTree newAnimal = new AnimalTree(scan.nextLine());
-			System.out.println("What question separates " + newAnimal.data() +  " from " + animalTree.visit(animalTree.left())+"?");
-			String seperateQuestion = scan.nextLine();
-			while (!finished){
-				System.out.println("What is the the correct answer for " + newAnimal.data() + "?");
-				String correctAnswer = scan.nextLine();
-				if (correctAnswer.equals("y")){
+		AnimalTree parentNode = animalTree.parent();
+		System.out.println("Dang! What was the animal?");
+		String answer = scan.nextLine();
+		AnimalTree newAnimal = new AnimalTree(answer);
+		System.out.println("What question separates " + newAnimal.data() +  " from " + animalTree.data()+"?");
+//		System.out.println("What question separates dog from " + animalTree.data()+"?");
+//		AnimalTree newAnimal = new AnimalTree("dog");
+		
+		String seperateQuestion = scan.nextLine();
+//		String seperateQuestion = "this question";
+		while (!finished){
+			System.out.println("What is the the correct answer for " + newAnimal.data() + "?");
+			String correctAnswer = scan.nextLine();
+			System.out.println(correctAnswer);
+			if (correctAnswer.equals("y")){
+				if (animalTree.parent().left() == animalTree){
 					AnimalTree newNode = new AnimalTree(seperateQuestion,animalTree,newAnimal);
-					animalTree.setLeft(newNode);
-					animalTree.writeFile("tree.txt");
-					finished = true;
-				}else if(correctAnswer.equals("n")){
-					AnimalTree oldAnimal = new AnimalTree(animalTree.visit(animalTree.left()));
-					AnimalTree newNode = new AnimalTree(seperateQuestion,newAnimal,oldAnimal);
-					animalTree.setLeft(newNode);
-			
-					animalTree.writeFile("tree.txt");
-					finished = true;
-				}else {
-					System.out.println("Just answer the stupid question!");
+					parentNode.setLeft(newNode);
+					newNode.writeFile(outputFile);
+
 				}
+				else {
+					AnimalTree newNode = new AnimalTree(seperateQuestion,animalTree,newAnimal);
+					parentNode.setRight(newNode);
+					System.out.println("hey right");
+					newNode.writeFile(outputFile);
+				}
+				System.out.println("hey3");
+				finished = true;
+			}else if(correctAnswer.equals("n")){
+				if (animalTree.parent().left() == animalTree){
+					AnimalTree newNode = new AnimalTree(seperateQuestion,newAnimal,animalTree);
+					parentNode.setLeft(newNode);
+					newNode.writeFile(outputFile);
+				}
+				else {
+					AnimalTree newNode = new AnimalTree(seperateQuestion,newAnimal,animalTree);
+					parentNode.setRight(newNode);
+					newNode.writeFile(outputFile);
+				}
+				finished = true;
+			}else {
+				System.out.println("Just answer the stupid question!");
 			}
 		}
-
-			//AnimalTree newTree = new AnimalTree(newAnimal);
-		//}
-		
-		//System.out.println(animalTree.isRoot()+" 3");
-		//System.out.println(animalTree.data()+" 3");
-		
-		
-		
+		System.out.println("hey2");
+		if (animalTree.isRoot()){
+		}
 			
+	}
 	}
 }
