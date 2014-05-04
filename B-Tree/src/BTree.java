65bf6168fc4317d21628bf88392d17f73ae1d8c1
Georgios Katsaounis <gkatsaounis@mendon.cs.middlebury.edu>
2014-05-04 17:59:51 -0400

    added Btree info

--- B-Tree/src/BTree.java
+++ B-Tree/src/BTree.java
@@ -26,4 +26,43 @@ public class BTree<Key extends Comparable<Key>, Value> {
 		return height;
 	}
 	
+	public Value get(Key key) {
+		
+		return search(root,key, height);
+	}
+	public Value search(Node n, Key key, int ht) {
+		Entries[] children = n.children;
+		
+		//external node
+		if (ht == 0) {
+			for(int i==0; i<n.data; i++) {
+				if(equal(key, children[i].key)) {
+					return (Value) children[i].value;
+				}
+			}
+		}
+		//internal node
+		else {
+			for(int i==0; i<n.data ; i++) {
+				if(i+1 == x.data || smaller(key, children[i+1].key)) {
+					return search(children[i].nextNode, key, ht-1);
+				}
+			}
+		}
+	}
+	
+	public Node split(Node n) {
+		halfChildren = maxChildren/2;
+		Node current = new Node(halfChildren);
+		n.data = halfChildren;
+		for(int i = 0; i < halfChildren; i++)
+	}
+	
+	public boolean equal(Comparable c1, Comparable c2) {
+		return c1.compareTo(c2) == 0;
+	}
+	
+	public boolean smaller(Comparable c1, Comparable c2) {
+		return c1.compareTo(c2) < 0;
+	}
 }
